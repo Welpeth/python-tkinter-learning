d4241d85d5e9ae1521fbf6316fabedd2e5d7bc89
Henrique José Dias Pereira <henriquejdpereira@gmail.com>
2024-07-19 16:47:45 -0300

    19/07/2024

--- tk-paintingame.py
+++ tk-paintingame.py
@@ -15,6 +15,17 @@ window.title('Painting Game')
 canvas = tk.Canvas(window, bg = 'white')
 canvas.pack()
 
+def brush_size_adjust(event):
+    global brush_size
+    if event.delta > 0:
+        brush_size += 4
+    else:
+        brush_size -= 4
+
+    brush_size = max(0, min(brush_size, 50))
+
 canvas.bind('<Motion>', draw_on_canvas)
+canvas.bind('<MouseWheel>', brush_size_adjust)
+
 
 window.mainloop()
